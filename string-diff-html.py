739ebfeb6c08f3c87363e378757be1398107d06b
laomai <laomai520@gmail.com>
2014-12-12 11:25:28 +0800

    字符串对比，输出HTML文档

--- string-diff-html.py
+++ string-diff-html.py
@@ -21,11 +21,3 @@ text2_lines = text2.splitlines()
 
 d = difflib.HtmlDiff()    # 创建Differ()对象
 print d.make_file(text1_lines, text2_lines)
-
-
-"""比较结果符号含义说明：
-'-'  包含在第一个序列行中，但不包含在第二个序列行
-'+'  包含在第二个序列行中，但不包含在第一个序列行
-' '  两个序列行一致
-'?'  标志两个序列行存在增量差异
-'^'  标志出两个序列存在的差异字符"""
